eaae2717e5483c6900ede971b60ccc41c7e984f3
Vidiic <jordividal1997@gmail.com>
2023-03-06 21:23:45 +0100

    drivers

--- src/components/Drivers.jsx
+++ src/components/Drivers.jsx
@@ -2,14 +2,27 @@ import { useState } from 'react'
 import data from '../local-json/drivers'
 
 
-export default function Drivers(){
+function CardDriver({driver}){
+    return (
+        <div className='card-driver'>
+            <div>{driver.permanentNumber}</div>
+            <div className='color'>{driver.color}</div>
+            <div>
+                <small>{driver.givenName}</small>
+                <h4 className='name'>{driver.familyName}</h4>
+            </div>
+        </div>
+    )
+}
+
+export default function Drivers() {
     const [drivers, _] = useState(data.drivers ?? []);
 
     return (
-        <ul>
+        <div className='content-drivers'>
             {
-                drivers.map((driver) => <li key={driver.driverId}>{driver.givenName}</li>)
+                drivers.map((d) => <CardDriver driver={d} />)
             }
-        </ul>
+        </div>
     )
 }
--- src/components/Races.jsx
+++ src/components/Races.jsx
@@ -1,14 +1,16 @@
 import data from '../local-json/races'
 import { useState } from 'react';
 
-export default function Races(){
+export default function Races() {
     const [races, _] = useState(data.races ?? []);
     console.log(races);
     return (
-        <ul>
-            {
-                races.map((race) => <li key={race.round}>{race.raceName}</li>)
-            }
-        </ul>
+        <div className='content-races'>
+            <ul>
+                {
+                    races.map((race) => <li key={race.round}>{race.raceName}</li>)
+                }
+            </ul>
+        </div>
     )
 }
